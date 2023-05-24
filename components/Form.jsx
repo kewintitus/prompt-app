180ec3c847a1d0d7049b6eba5b8ee4397a0506a4
Kewin Titus <kewintitus@gmail.com>
2023-05-24 20:14:20 +0530

    create post page Init

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompts with the world, and let ypur
+        imagination run wild with any AI Platform
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label htmlFor="">
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            value={post.prompt}
+            onChange={(e) => {
+              setPost({ ...post, prompt: e.target.value });
+            }}
+            placeholder="Write your prompt"
+            required
+            className="form_textarea"
+          ></textarea>
+        </label>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
